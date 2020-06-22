4267d977860443d9cb21e2e6050ee304654a1717
claylton <clayltonsp@hotmail.com>
2020-06-22 16:26:23 -0300

    iniciando testes

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -2,6 +2,14 @@ namespace Payment.Context.Domain.Entities
 {
     public class Student
     {
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+        }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
