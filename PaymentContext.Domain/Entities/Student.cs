13f93105ac3a01254e4c575f5d4c3a9a7a5fc20c
claylton <clayltonsp@hotmail.com>
2020-06-23 17:27:27 -0300

    Arrumando as entidades

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,22 +1,37 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
+        private IList<Subscription> _subscriptions;
         public Student(string firstName, string lastName, string document, string email)
         {
             FirstName = firstName;
             LastName = lastName;
             Document = document;
             Email = email;
+            _subscriptions = new List<Subscription>();
         }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Document { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public List<Subscription> Subscriptions { get; set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            // 1 - Opção se já tiver uma assinatura ativa, cancela
+
+            // 2 - Opção Cancela todas as assinaturas e coloca esta
+            //     como principal
+            foreach (var subs in Subscriptions)
+                subs.Inactivate();
+
+            _subscriptions.Add(subscription);
+        }
     }
 }
