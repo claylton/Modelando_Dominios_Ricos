a83f0782131ff2008821528e9ae09016728d03d7
claylton <clayltonsp@hotmail.com>
2020-07-07 19:22:26 -0300

    Adicionando um exemplo de Fail Fast Validations

--- PaymentContext.Domain/Commands/CreateBoletoSubscription.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscription.cs
@@ -1,10 +1,14 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreateBoletoSubscriptionCommand
+    public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
     {
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
@@ -28,5 +32,15 @@ namespace PaymentContext.Domain.Commands
         public string Country { get; set; }
         public string ZipCode { get; set; }
 
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
+                .HasMaxLen(LastName, 40, "Name.LastName", "Sobrenome deve conter até 40 caracteres")
+);
+        }
     }
 }
